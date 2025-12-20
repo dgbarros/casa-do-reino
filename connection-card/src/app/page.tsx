dc737b160e543b9863b43ebb5baf87829b12ca7d
Douglas Barros <douglas.barros2000@outlook.com>
2025-12-19 23:27:02 -0300

    feat: Criando arquitetura de pastas em um nível de pequena escala

---
+++ connection-card/src/app/page.tsx
@@ -0,0 +1,5 @@
+import Inicio from "@/src/components/Inicio";
+
+export default function Home() {
+  return <Inicio/>
+}
---
+++ connection-card/src/components/Inicio.tsx
@@ -0,0 +1,12 @@
+const Inicio = () => {
+    return (
+        <div>
+            <h1>
+                Início
+            </h1>
+        </div>
+    )
+}
+
+
+export default Inicio;
