f8273b7f908163e66dc00ed35a6772fdf0c29bbb
Douglas Barros <douglas.barros2000@outlook.com>
2025-12-23 23:23:52 -0300

    fix: corrigindo layout do formulário e inserindos novas redes sociais

--- connection-card/src/components/FormularioMembro.tsx
+++ connection-card/src/components/FormularioMembro.tsx
@@ -4,13 +4,12 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-// Importar sua função de serviço aqui depois
 
-// Schema de validação (Pode mover para lib/validators.ts)
 const schema = z.object({
   nome: z.string().min(3, "O nome precisa ter pelo menos 3 letras"),
   telefone: z.string().min(10, "Informe um telefone válido"),
   pedidoOracao: z.string().optional(),
+  interesses: z.array(z.string()).min(1, "Selecione pelo menos um interesse")
 });
 
 type FormData = z.infer<typeof schema>;
@@ -19,16 +18,18 @@ export default function FormularioMembro() {
   const [enviando, setEnviando] = useState(false);
   const [sucesso, setSucesso] = useState(false);
 
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
   const onSubmit = async (data: FormData) => {
     setEnviando(true);
     try {
-      // Aqui chamaremos seu services/formulario.ts
-      // await enviarFormulario(data); 
-      console.log(data); // Teste por enquanto
+      console.log("Realizando envio do formulário",data)
       setSucesso(true);
     } catch (error) {
       alert("Erro ao enviar");
@@ -43,10 +44,14 @@ export default function FormularioMembro() {
         <div className="w-16 h-16 bg-green-100 rounded-full flex items-center justify-center mb-4">
           <span className="text-2xl"></span>
         </div>
-        <h2 className="text-xl font-bold text-gray-900">Recebemos seu contato!</h2>
-        <p className="text-gray-500 mt-2">O pastor entrará em contato em breve.</p>
-        <button 
-          onClick={() => setSucesso(false)} 
+        <h2 className="text-xl font-bold text-gray-900">
+          Recebemos seu contato!
+        </h2>
+        <p className="text-gray-500 mt-2">
+          O pastor entrará em contato em breve.
+        </p>
+        <button
+          onClick={() => setSucesso(false)}
           className="mt-6 text-black font-semibold underline"
         >
           Voltar
@@ -57,39 +62,82 @@ export default function FormularioMembro() {
 
   return (
     <div className="px-6 pt-10 animate-in slide-in-from-right duration-500">
-      <h2 className="text-2xl font-bold text-gray-900 mb-1">Cartão de Conexão</h2>
-      <p className="text-gray-500 text-sm mb-8">Queremos conhecer você melhor.</p>
-
+      <h2 className="text-2xl font-bold text-gray-900 mb-1">
+        Cartão de Conexão
+      </h2>
+     
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
-        
         <div className="space-y-1">
-          <label className="text-xs font-semibold text-gray-700 uppercase">Nome Completo</label>
+          <label className="text-xs font-semibold text-gray-700 uppercase">
+            Nome
+          </label>
           <input
             {...register("nome")}
-            className="w-full p-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-black focus:border-transparent outline-none transition-all"
+            className=" text-gray-900 w-full p-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-black focus:border-transparent outline-none transition-all"
             placeholder="Seu nome"
           />
-          {errors.nome && <span className="text-red-500 text-xs">{errors.nome.message}</span>}
+          {errors.nome && (
+            <span className="text-red-500 text-xs">{errors.nome.message}</span>
+          )}
         </div>
 
+        {/* TELEFONE */}
         <div className="space-y-1">
-          <label className="text-xs font-semibold text-gray-700 uppercase">Telefone / WhatsApp</label>
+          <label className="text-xs font-semibold text-gray-700 uppercase">
+            Telefone
+          </label>
           <input
             {...register("telefone")}
-            className="w-full p-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-black focus:border-transparent outline-none transition-all"
+            className=" text-gray-900 w-full p-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-black focus:border-transparent outline-none transition-all"
             placeholder="(11) 99999-9999"
           />
-          {errors.telefone && <span className="text-red-500 text-xs">{errors.telefone.message}</span>}
+          {errors.telefone && (
+            <span className="text-red-500 text-xs">
+              {errors.telefone.message}
+            </span>
+          )}
         </div>
 
         <div className="space-y-1">
-          <label className="text-xs font-semibold text-gray-700 uppercase">Pedido de Oração (Opcional)</label>
+          <label className="text-xs font-semibold text-gray-700 uppercase">
+            Pedido de oração ou comentário
+          </label>
           <textarea
             {...register("pedidoOracao")}
             rows={4}
-            className="w-full p-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-black focus:border-transparent outline-none transition-all resize-none"
+            className=" text-gray-900 w-full p-3 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-black focus:border-transparent outline-none transition-all resize-none"
             placeholder="Como podemos orar por você?"
           />
+        </div>
+
+        <div className="pt-2">
+          <p className="text-xs font-semibold text-gray-700 uppercase mb-3">
+            Interesse em:
+          </p>
+
+          <div className="grid grid-cols-2 gap-y-3 gap-x-4">
+            {[
+              "Visita Pastoral",
+              "Batismo",
+              "Membresia",
+              "Aconselhamento Pastoral",
+              "Voluntariado",
+              "Consagração de Criança",
+            ].map((item) => (
+              <label
+                key={item}
+                className="flex items-center gap-2 text-sm text-gray-700"
+              >
+                <input
+                  type="checkbox"
+                  {...register("interesses")}
+                  value={item}
+                  className="text-gray-900 h-4 w-4 border-gray-300 rounded text-black focus:ring-black"
+                />
+                {item}
+              </label>
+            ))}
+          </div>
         </div>
 
         <button
